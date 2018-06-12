13abe9b799ae68898057699f3f60cd8e21653c56
OVRLDCKN <40148172+OVRLDCKN@users.noreply.github.com>
2018-06-12 09:27:12 +0200

    Added "start_client" class, that starts the game, or creates a User, if there is no User Added "game" class, that starts the actual game

--- game_client.py
+++ game_client.py
@@ -1,6 +1,42 @@
 #I have finally decided, that I will do a Game! (Multiplayer with every PC in your Local Network)
 #Importing Modules
 from tools import *
+from tkinter import *
+import pygame
+from pygame.locals import *
+
+class start_client:
+    def __init__(self):
+        self.username = ""
+        self.winlist = []
+    def create_player(self):
+        FILE = class_read_write("player.txt")
+        try:
+            [self.username,self.winlist] = FILE.read()
+            GAME = game(self.username, self.winlist)
+            GAME.start()
+        except FileNotFoundError:
+
+            def create():
+                self.username = entry_username.get()
+                if self.username != "":
+                    self.winlist = []
+                    locallist = []
+                    locallist.append(self.username)
+                    locallist.append(self.winlist)
+                    FILE.write(locallist)
+                    GAME = game(self.username,self.winlist)
+                    GAME.start()
+                    window.quit()
+
+            window = Tk()
+            label_username = Label(window, text="Username")
+            label_username.pack()
+            entry_username = Entry(window)
+            entry_username.pack()
+            button_create = Button(window, text="Create!", command=lambda : create())
+            button_create.pack()
+            window.mainloop()
 
 #TODO create user data, If not exsisting
 
@@ -15,3 +51,21 @@ from tools import *
 #TODO make "checkwin" function or class, that checks, If one player has won
 
 #TODO make "drawgamefield" function or class, that draws the game Field
+class game:
+    def __init__(self,username,winlist):
+        self.username = username
+        self.winlist = winlist
+    def start(self):
+        pygame.init()
+        pygame.display.set_mode((800,600))
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+            pygame.display.update()
+
+CLIENT = start_client()
+CLIENT.create_player()
