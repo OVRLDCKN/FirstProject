271057ebfa366dd97b1e49499e8d94217f04284a
OVRLDCKN <40148172+OVRLDCKN@users.noreply.github.com>
2018-06-12 08:54:43 +0200

    I'm going to make a game! Added Game Server File Added Game Client File

---
+++ game_client.py
@@ -0,0 +1,17 @@
+#I have finally decided, that I will do a Game! (Multiplayer with every PC in your Local Network)
+#Importing Modules
+from tools import *
+
+#TODO create user data, If not exsisting
+
+#TODO connect to server
+
+#TODO send user data
+
+#TODO retrieve opponent data
+
+#TODO make "place" function or class, that places on a field and sends the Data to the Server
+
+#TODO make "checkwin" function or class, that checks, If one player has won
+
+#TODO make "drawgamefield" function or class, that draws the game Field
---
+++ game_server.py
@@ -0,0 +1,13 @@
+#I have finally decided, that I will do a Game! (Multiplayer with every PC in your Local Network)
+#Importing Modules
+from tools import *
+
+#TODO start game server
+
+#TODO connect client to server
+
+#TODO receive user data
+
+#TODO send opponent data
+
+#TODO make "checkwin" function or class, that checks, If one player has won
